a8b61af3ad05aca63119b3d841598f0560b2d80a
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-17 09:43:21 +0200

    Declaring string variables

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code below this line
+    var myFirstName = "firstName";
+    var myLastName = "lastName";
+    var myHomeTown = "homeTown"
+    // Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
