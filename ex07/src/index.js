033b28b9f50537df9ce08e72fb126d9ea0484fff
Zekerijah <zekerijahsehic1@gmail.com>
2021-09-17 09:41:06 +0200

    Finding a remainder

---
+++ ex07/src/index.js
@@ -0,0 +1,11 @@
+function main(numOne, numTwo){
+    // Only change code below this line
+    var reminder = numOne % numTwo;
+    // Only change code above this line
+    return reminder;
+}
+
+console.log(main(5,2));
+console.log(main(15,4));
+console.log(main(102,25));
+module.exports = main;
